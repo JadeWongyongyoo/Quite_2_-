14f910d2f6be9109c9ae28e31203f6db74a87d9c
JadeWongyoungyoo <nawaf.w1@gmail.com>
2021-09-18 14:20:06 +0700

    Hard : (

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Quit_2
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+
+        }
+    }
+    class Flower
+    {
+        public int NumberPlant;
+
+        public string NamePlant;
+
+        public string Description;
+
+        public string hight;
+
+        public string Treecircumference;
+
+        public Flower(string valuesizeNamePlant, string valueDescription, string valurhight, string valueTreecircumference)
+        {
+            NamePlant = valuesizeNamePlant;
+
+            Description = valueDescription;
+
+            hight = valurhight;
+
+            Treecircumference = valueTreecircumference;
+            
+           
+        }
+        class FlowerCart
+        {
+            public List<Flower> CartFlower;
+            public FlowerCart()
+            {
+                FlowerCart = new List<Flower>();
+                
+            }
+            public void PrintFlower()
+            {
+
+                foreach (Flower Flowerorder in CartFlower)
+
+                {
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    Console.WriteLine();
+                    
+                }
+            }
+        }
+    }
+
+}
